f24ec2761d7297156c81c574c86a790cc6899ccf
Ruslan Pitsik <rs.pitsik@gmail.com>
2023-05-22 07:23:43 +0300

    adding work operations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,58 +1,89 @@
 import './../styles/index.scss';
 
-let [operand1, operand2] = [0, 0];
-let firstFilled = false;
+let [operand1, operand2] = [0, null];
 let unary = undefined;
 
 const keyBoard = document.querySelector('.calc__keyboard');
 
+const actionElem = document.querySelector('.calc__what');
+actionElem.textContent = String(+operand1);
+
+const calcField = document.querySelector('.calc__field');
+calcField.textContent = operand1;
+
 keyBoard.addEventListener('click', e => {
 	// Первое число
 	if (
-		firstFilled === false &&
+		e.target.closest('.calc__button') &&
 		unary === undefined &&
+		operand2 === null &&
 		['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(
 			e.target.closest('.calc__button').textContent.trim()
 		)
 	) {
-		console.log('Ввели 1 число');
-		operand1 += e.target.textContent;
+		operand1 = String(operand1) + e.target.textContent;
+
+		actionElem.textContent = String(+operand1);
 	}
 
 	// Унарный
 	else if (
+		e.target.closest('.calc__button') &&
 		['+', '-', '/', '%', '*'].includes(
 			e.target.closest('.calc__button').dataset.unary
 		) &&
-		operand1 !== 0
+		operand2 === null
 	) {
-		console.log('Унарный');
-		firstFilled = true;
 		unary = e.target.closest('.calc__button').dataset.unary;
+
+		actionElem.textContent = `${
+			actionElem.textContent.split(' ')[0]
+		} ${unary} `;
 	}
 	// Второе число
 	else if (
-		firstFilled === true &&
 		unary !== undefined &&
-		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(+e.target.textContent)
+		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(+e.target.textContent) &&
+		e.target.closest('.calc__button') &&
+		e.target.closest('.calc__button').textContent !==
+			'\n\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\n\t\t\t\t'
 	) {
-		console.log('Ввели 2 число');
-		operand2 += e.target.textContent;
+		console.log(22);
+		operand2 = String(+operand2) + e.target.textContent;
+
+		actionElem.textContent = `${
+			actionElem.textContent.split(' ')[0]
+		} ${unary} ${+operand2}`;
 	}
 	// ====
 	else if (
 		e.target.textContent.trim() === '=' &&
-		firstFilled === true &&
-		operand1 !== 0 &&
-		operand2 !== 0
+		operand2 !== null &&
+		unary !== undefined
 	) {
-		console.log('Резултат', operation(unary));
-		operand1 = operation(unary);
-		operand2 = 0;
-		unary = undefined;
+		equally();
 	}
-	// Если продолжаем
-	// else if () {}
+	// Полнный сброс
+	else if (e.target.textContent.trim() === 'C') {
+		fullReset();
+	}
+	// Неполный сброс
+	else if (e.target.textContent.trim() === 'CE') {
+		lastReset();
+	}
+	// Если продолжать
+	else if (
+		unary !== undefined &&
+		operand2 !== null &&
+		e.target.closest('.calc__button') &&
+		['+', '-', '/', '%', '*'].includes(
+			e.target.closest('.calc__button').dataset.unary
+		)
+	) {
+		equally();
+		unary = e.target.closest('.calc__button').dataset.unary;
+		actionElem.textContent = `${operand1} ${unary} `;
+	}
 });
 
 function operation(key) {
@@ -67,10 +98,32 @@ function operation(key) {
 			return +operand1 / +operand2;
 			break;
 		case '*':
-			return +operand1 / +operand2;
+			return +operand1 * +operand2;
 			break;
 		case '%':
 			return +operand1 % +operand2;
 			break;
 	}
+}
+
+function equally() {
+	actionElem.textContent = String(operation(unary));
+	calcField.textContent = operation(unary);
+	operand1 = operation(unary);
+	operand2 = null;
+	unary = undefined;
+}
+
+function fullReset() {
+	actionElem.textContent = String(0);
+	operand1 = 0;
+	operand2 = null;
+	unary = undefined;
+	calcField.textContent = operand1;
+}
+
+function lastReset() {
+	actionElem.textContent = actionElem.textContent.split(' ')[0];
+	operand2 = null;
+	unary = undefined;
 }
