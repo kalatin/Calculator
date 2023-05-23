003858a6eff2bd2c0e175b1b6919a0f2be21e054
Ruslan Pitsik <rs.pitsik@gmail.com>
2023-05-23 06:41:20 +0300

    add keyboard click

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,24 @@
 import './../styles/index.scss';
 
-let [operand1, operand2] = [1, null];
+// Операнды 1-2
+let [operand1, operand2] = [0, null];
+// Унарный оператор
 let unary = undefined;
 
+// Клавиатура
 const keyBoard = document.querySelector('.calc__keyboard');
 
+// Поле отображения операции
 const actionElem = document.querySelector('.calc__what');
 actionElem.textContent = String(+operand1);
 
+// Поле отоброжения результата
 const calcField = document.querySelector('.calc__field');
 calcField.textContent = operand1;
 
-keyBoard.addEventListener('click', e => {
+// Нажатие на клавиатуру
+keyBoard.addEventListener('click', handleClick);
+function handleClick(e) {
 	// Первое число
 	if (
 		e.target.closest('.calc__button') &&
@@ -22,15 +29,14 @@ keyBoard.addEventListener('click', e => {
 		)
 	) {
 		operand1 = String(operand1) + e.target.textContent;
-
 		actionElem.textContent = String(+operand1);
 	}
 	// Точка с запятой - 1
 	else if (
-		!String(operand1).includes('.') &&
 		e.target.closest('.calc__button') &&
 		e.target.closest('.calc__button').textContent === ',' &&
-		unary === undefined
+		unary === undefined &&
+		!String(operand1).includes('.')
 	) {
 		operand1 = String(operand1) + '.';
 		actionElem.textContent = actionElem.textContent + '.';
@@ -45,11 +51,11 @@ keyBoard.addEventListener('click', e => {
 		operand2 === null
 	) {
 		unary = e.target.closest('.calc__button').dataset.unary;
-
 		actionElem.textContent = `${+actionElem.textContent.split(
 			' '
 		)[0]} ${unary} `;
 	}
+
 	// плюс/минус
 	else if (
 		e.target.closest('.calc__button') &&
@@ -65,13 +71,14 @@ keyBoard.addEventListener('click', e => {
 			actionElem.textContent = operand1;
 		}
 	}
+
 	// Второе число
 	else if (
-		unary !== undefined &&
-		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(+e.target.textContent) &&
 		e.target.closest('.calc__button') &&
 		e.target.closest('.calc__button').textContent !==
-			'\n\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\n\t\t\t\t'
+			'\n\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\n\t\t\t\t' &&
+		unary !== undefined &&
+		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(+e.target.textContent)
 	) {
 		operand2 === null
 			? (operand2 = String(+operand2) + e.target.textContent)
@@ -83,10 +90,10 @@ keyBoard.addEventListener('click', e => {
 	}
 	// Точка с запятой - 2
 	else if (
-		!String(operand2).includes('.') &&
 		e.target.closest('.calc__button') &&
 		e.target.closest('.calc__button').textContent === ',' &&
-		unary !== undefined
+		unary !== undefined &&
+		!String(operand2).includes('.')
 	) {
 		operand2 = String(+operand2) + '.';
 		actionElem.textContent = `${+actionElem.textContent.split(
@@ -102,7 +109,6 @@ keyBoard.addEventListener('click', e => {
 	) {
 		equally();
 	}
-
 	// Полнный сброс
 	else if (e.target.textContent.trim() === 'C') {
 		fullReset();
@@ -114,30 +120,20 @@ keyBoard.addEventListener('click', e => {
 
 	// Если продолжать
 	else if (
-		unary !== undefined &&
-		operand2 !== null &&
 		e.target.closest('.calc__button') &&
 		['+', '-', '/', '%', '*'].includes(
 			e.target.closest('.calc__button').dataset.unary
-		)
+		) &&
+		unary !== undefined &&
+		operand2 !== null
 	) {
 		equally();
 		unary = e.target.closest('.calc__button').dataset.unary;
 		actionElem.textContent = `${operand1} ${unary} `;
 	}
-});
+}
 
 function operation(key) {
-	if (String(operand1).split('.').length > 1) {
-		operand1 = (+operand1).toFixed(
-			String(operand1).split('.')[1].length
-		);
-	}
-	if (String(operand2).split('.').length > 1) {
-		operand2 = (+operand2).toFixed(
-			String(operand2).split('.')[1].length
-		);
-	}
 	switch (key) {
 		case '+':
 			{
@@ -213,3 +209,102 @@ function lastReset() {
 	operand2 = null;
 	unary = undefined;
 }
+
+// Нажатие на клавиши
+window.addEventListener('keydown', e => {
+	// Первое число
+	if (
+		['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(e.key) &&
+		unary === undefined &&
+		operand2 === null
+	) {
+		operand1 = String(operand1) + e.key;
+		actionElem.textContent = String(+operand1);
+	}
+	// Точка с запятой - 1
+	else if (
+		e.key === ',' &&
+		unary === undefined &&
+		!String(operand1).includes('.')
+	) {
+		operand1 = String(operand1) + '.';
+		actionElem.textContent = actionElem.textContent + '.';
+	}
+
+	// Унарный
+	else if (
+		['+', '-', '/', '%', '*'].includes(e.key) &&
+		operand2 === null
+	) {
+		unary = e.key;
+		actionElem.textContent = `${+actionElem.textContent.split(
+			' '
+		)[0]} ${unary} `;
+	}
+
+	// плюс/минус
+	else if (e.code == 'KeyP') {
+		if (operand2 !== null && unary !== undefined) {
+			operand2 *= -1;
+			actionElem.textContent = `${+actionElem.textContent.split(
+				' '
+			)[0]} ${unary} ${String(operand2)}`;
+		} else if (operand2 === null && unary === undefined) {
+			operand1 *= -1;
+			actionElem.textContent = operand1;
+		}
+	}
+
+	// Второе
+	else if (
+		unary !== undefined &&
+		['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(e.key)
+	) {
+		operand2 === null
+			? (operand2 = String(+operand2) + e.key)
+			: (operand2 = String(operand2) + e.key);
+
+		actionElem.textContent = `${+actionElem.textContent.split(
+			' '
+		)[0]} ${unary} ${String(+operand2)}`;
+	}
+	// Точка с запятой - 2
+	else if (
+		e.key === ',' &&
+		unary !== undefined &&
+		!String(operand2).includes('.')
+	) {
+		operand2 = String(+operand2) + '.';
+		actionElem.textContent = `${+actionElem.textContent.split(
+			' '
+		)[0]} ${unary} ${String(+operand2 + '.')}`;
+	}
+
+	// ========
+	else if (
+		e.key === 'Enter' &&
+		operand2 !== null &&
+		unary !== undefined
+	) {
+		equally();
+	}
+	// Полнный сброс
+	else if (e.code === 'KeyC') {
+		fullReset();
+	}
+	// Неполный сброс
+	else if (e.code === 'KeyE') {
+		lastReset();
+	}
+
+	// Если продолжать
+	else if (
+		['+', '-', '/', '%', '*'].includes(e.key) &&
+		unary !== undefined &&
+		operand2 !== null
+	) {
+		equally();
+		unary = e.key;
+		actionElem.textContent = `${operand1} ${unary} `;
+	}
+});
